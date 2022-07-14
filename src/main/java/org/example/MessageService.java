006292c8c4d9f1b65230ba1b33aa544c4862f498
rajsuman-choubey <Rajsuman.choubey2022@gmail.com>
2022-07-14 14:38:25 +0200

    Task-2 Add change in MessageService

--- src/main/java/org/example/MessageService.java
+++ src/main/java/org/example/MessageService.java
@@ -3,7 +3,7 @@ package org.example;
 import jakarta.ws.rs.core.Response;
 
 public interface MessageService {
-  public String getHello() ;
- public long curiosityDateConversion(String date);
+  String getHello() ;
+ long curiosityDateConversion(String date);
    public String getTodayDate();
 }

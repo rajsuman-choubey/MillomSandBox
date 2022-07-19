d009c1f403c2664222c67f3d834e5f67e3657ce3
rajsuman-choubey <Rajsuman.choubey2022@gmail.com>
2022-07-19 09:12:43 +0200

    Task-2 Add changes MyResourcTest

--- src/test/java/org/example/MessageServiceImplTest.java
+++ src/test/java/org/example/MessageServiceImplTest.java
@@ -1,4 +1,31 @@
+package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 class MessageServiceImplTest {
-  
+
+  MessageServiceImpl messageServiceImpl = new MessageServiceImpl();
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  void testCuriosityDateConversion() {
+    assertEquals(3504, messageServiceImpl.curiosityDateConversion("2022-06-15"));
+  }
+
+  @Test
+  void curiosityDateConversionThrowsExceptionIfDateInvalidFormat() {
+    String date = "20-06-15";
+    assertThrows(RuntimeException.class, () -> messageServiceImpl.curiosityDateConversion(date));
+  }
 }
--- src/test/java/org/example/MyResourceTest.java
+++ src/test/java/org/example/MyResourceTest.java
@@ -1,6 +1,8 @@
 package org.example;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
 import java.net.URI;
@@ -12,8 +14,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+
 @RunWith(MockitoJUnitRunner.class)
 class MyResourceTest {
+
   @Mock
   private MessageService messageServiceMock;
   private MessageService msgServiceImpl;
@@ -22,7 +26,7 @@ class MyResourceTest {
   MyResource myResource = new MyResource();
 
   @BeforeEach
-  void setUp() throws Exception{
+  void setUp() throws Exception {
     MockitoAnnotations.initMocks(this);
     msgServiceImpl = new MessageServiceImpl();
   }
@@ -30,10 +34,10 @@ class MyResourceTest {
   @Test
   @DisplayName("CONVERT EARTH DATE TO CURIOSITY SOL DATE")
   void testCuriosityDateConversion() {
-  Response response = Response.
-      status(Response.Status.OK)
-      .entity(msgServiceImpl.curiosityDateConversion("2022-06-15"))
-    .build();
+    Response response = Response.
+        status(Response.Status.OK)
+        .entity(msgServiceImpl.curiosityDateConversion("2022-06-15"))
+        .build();
 
     when(messageServiceMock.curiosityDateConversion("2022-06-15")).thenReturn(3504L);
     response = myResource.curiosityDateConversion("2022-06-15");
@@ -53,16 +57,18 @@ class MyResourceTest {
     assertEquals(response.getStatus(), 200);
     assertEquals(response.getEntity(), actual);
   }
+
   @Test
   void TestCuriosityDateConversionThrowsException() {
-    String date ="2022-";
+    String date = "2022-";
     when(messageServiceMock.curiosityDateConversion(date)).thenThrow(new RuntimeException());
     Response response = myResource.curiosityDateConversion(date);
     assertEquals(response.getStatus(), 400);
-    assertEquals(response.getEntity(), null );
+    assertEquals(response.getEntity(), null);
   }
+
   private static URI getBaseURI(String path) {
-    return UriBuilder.fromUri("http://localhost:8080/"+path).build();
+    return UriBuilder.fromUri("http://localhost:8080/" + path).build();
   }
 }
 
